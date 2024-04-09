782ea07c2927672d35526168af1253cb8e3fb514
aariandhanani <47797960+aariandhanani@users.noreply.github.com>
2024-04-09 02:58:02 -0400

    Add files via upload

--- js/js-file.js
+++ js/js-file.js
@@ -1,15 +1,18 @@
 window.addEventListener("load", function() {
 	console.log("Window open");
+    document.querySelector("#night").style.color = "#ffee00";
 });
 
 document.querySelector("#night").addEventListener("click", function() {
     console.log(document.body.style.backgroundColor);
     if (document.body.style.backgroundColor == "" || document.body.style.backgroundColor == "white") {
         document.body.style.backgroundColor = "#2d1f5b";
+        document.querySelector("#night").style.color = "#2d1f5b";
         console.log("changed to night");
     }
     else {
         document.body.style.backgroundColor = "white";
+        document.querySelector("#night").style.color = "#ffee00";
         console.log("changed to day");
     }
     console.log(document.body.style.backgroundColor);
